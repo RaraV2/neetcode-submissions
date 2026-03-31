25607067c405e384a66fbc25e87ff066f6fff54c
RaraV2 <87941142+RaraV2@users.noreply.github.com>
2026-03-31 14:55:07 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        if (nums.empty()) return 0;
+
+        set<int> k(nums.begin(), nums.end());
+
+        int res = 1;
+        int temp_res = 1;
+
+        auto it = k.begin();
+        auto next_it = it;
+        ++next_it;
+
+        while (next_it != k.end()) //to make sure it doesnt run out of bounds if it != k.end() it+1 would have have run outof bounds 
+        {
+            if (*next_it == *it + 1) {
+                temp_res++;
+            } else {
+                temp_res = 1;
+            }
+
+            res = max(res, temp_res);
+
+            ++it;
+            ++next_it;
+        }
+
+        return res;
+    }
+};
