15d8802f53d75f42fdaba98f5a4ba54df0b85c90
RaraV2 <87941142+RaraV2@users.noreply.github.com>
2026-08-02 18:03:31 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int pos{0};
+        unordered_map<int,int> m;
+        m.reserve(nums.size());
+        for(auto const &x: nums){   
+            int key = target-x;                   
+            if(m.count(key)) return {m[key],pos};
+            m.emplace(x,pos);
+            pos++;
+        } 
+    }
+};
