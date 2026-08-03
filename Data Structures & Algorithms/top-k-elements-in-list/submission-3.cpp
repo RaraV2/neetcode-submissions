a597d0a86fbf0f91cc7dd49af61c7077483f5291
RaraV2 <87941142+RaraV2@users.noreply.github.com>
2026-08-03 19:33:25 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> map{};
+        vector<vector<int>> bucket(nums.size()+1);
+        vector<int> answer{};
+
+        for(const auto &x : nums){
+            map[x]++;
+        }
+
+        for(const auto &m : map){
+            bucket[m.second].push_back(m.first);
+        }
+
+        for(auto it = bucket.rbegin(); it != bucket.rend(); ++it){
+
+            for(auto &z : *it){
+                if(k == 0) break;
+                answer.push_back(z);
+                --k;
+            }
+            if(k == 0) break;
+        }
+        return answer;
+    }
+};
