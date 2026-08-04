b65ebf20fbb385d79668a5234a7067cdf30d6295
RaraV2 <87941142+RaraV2@users.noreply.github.com>
2026-08-04 20:45:19 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        vector<int> prefixArray(nums.size(),1);
+        vector<int> postfixArray(nums.size(),1);
+
+        for(int i = 0; i < nums.size() - 1; ++i) {
+            prefixArray[i+1] *= prefixArray[i] * nums[i]; 
+        }
+
+        for(int i = nums.size() - 1; i > 0; --i){
+            postfixArray[i-1] = postfixArray[i] * nums[i];
+        } 
+
+        vector<int> answer;
+        for(int i = 0; i < nums.size(); ++i){
+            answer.push_back(prefixArray[i] * postfixArray[i]);
+        }
+
+        return answer;
+    }
+};
